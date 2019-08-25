49d52faa35b1b01e623e09069520b301f0bf3681
Georgi Petrushev <figoto660@gmail.com>
2019-08-25 15:03:44 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,24 @@
+var array;
+
+function forLoop(array){
+  for(let i = 0 ;i<25 ; i++)
+  {
+    if(i==1)
+    {
+     array.push("I am 1 strange loop.");
+    }
+    else
+    array.push(`I am ${i} strange loops.`);
+  }
+  return array;
+}
+var n;
+function whileLoop(n){
+  let count= n;
+  while(count > 0){
+    console.log(count--);
+  }
+  return "done";
+  
+}
+function doWhileLoop()
